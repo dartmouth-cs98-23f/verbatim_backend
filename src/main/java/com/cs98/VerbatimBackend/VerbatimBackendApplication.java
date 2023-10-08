f0aedaa0e137bd9dae6ce519456a4141edc5c211
Eric Richardson <eric.t.richardson.24@dartmouth.edu>
2023-10-08 17:00:38 -0400

    First commit

---
+++ src/main/java/com/cs98/VerbatimBackend/VerbatimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cs98.VerbatimBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbatimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbatimBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs98/VerbatimBackend/VerbatimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs98.VerbatimBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbatimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
