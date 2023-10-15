357e9e7c3dbc7e435add9dffdc6bbbf511384892
Dahlia Igiraneza <97261557+DahliaTheCoder@users.noreply.github.com>
2023-10-15 13:37:07 -0400

    Update TestController.java

--- src/main/java/com/cs98/VerbatimBackend/TestController.java
+++ src/main/java/com/cs98/VerbatimBackend/TestController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+/**
+ * Setting origins to "*" means that any origin is allowed to make requests to our Spring Boot server.
+ * This completely disables CORS protection, which can be a security risk in production environments.
+ * Therefore, in the future, we will have to specify the allowed origins explicitly or apply more fine-grained CORS configuration in a production environment.
+ */
+@CrossOrigin(origins = "*") 
 @RestController
 public class TestController {
 
