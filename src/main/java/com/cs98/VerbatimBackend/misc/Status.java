9fcafbb095203f9caa86373716ddcccaf981acbf
Rydak87 <76986782+Rydak87@users.noreply.github.com>
2023-11-05 17:40:15 -0500

    status codes for authentication

--- src/main/java/com/cs98/VerbatimBackend/misc/Status.java
+++ src/main/java/com/cs98/VerbatimBackend/misc/Status.java
@@ -5,4 +5,6 @@ public class Status {
     public static final int USER_NOT_FOUND = 460;
     public static final int USERNAME_TAKEN = 461;
     public static final int EMAIL_TAKEN = 462;
+    public static final int WRONG_PASSWORD = 463;
+
 }
