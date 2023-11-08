076b6d80f2a71b761dd73a6310e6b91bf5677150
Eric Richardson <eric.t.richardson.24@dartmouth.edu>
2023-11-08 13:24:47 -0500

    total responses fix

--- src/main/java/com/cs98/VerbatimBackend/controller/GlobalChallengeController.java
+++ src/main/java/com/cs98/VerbatimBackend/controller/GlobalChallengeController.java
@@ -53,6 +53,7 @@ public class GlobalChallengeController {
                     .q1(dailyChallenge.getQ1().getContent())
                     .q2(dailyChallenge.getQ2().getContent())
                     .q3(dailyChallenge.getQ3().getContent())
+                    .totalResponses(globalChallengeUserResponseRepository.countByGlobalChallengeId(dailyChallenge.getId()))
                     .categoryQ1(dailyChallenge.getQ1().getCategory().getTitle())
                     .categoryQ2(dailyChallenge.getQ2().getCategory().getTitle())
                     .categoryQ3(dailyChallenge.getQ3().getCategory().getTitle())
