f92094f60b4964b847d38191396a181020f7cad5
Eric Richardson <eric.t.richardson.24@dartmouth.edu>
2024-02-23 12:07:22 -0500

    revert

--- src/main/java/com/cs98/VerbatimBackend/model/UserGroupJunction.java
+++ src/main/java/com/cs98/VerbatimBackend/model/UserGroupJunction.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class UserGroupJunction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @ManyToOne
